7421b03fc31ed94e2167195981242ca2cd4ff5d0
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-08-25 19:52:06 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,9 @@
 from add_0 import add
 a = 1
 b = 2
-if __name__ == "__main__":
-    print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))))
+#!/usr/bin/python3
+def add(a, b):
+    Args:
+        a: 1
+        b: 2
+            return (a + b)
