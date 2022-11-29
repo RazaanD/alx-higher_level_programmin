ac3d7cc813a9017412878c5e18f36e76a52b0483
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-11-30 01:25:12 +0200

    Create 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
