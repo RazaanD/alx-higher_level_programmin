32891895aadd93b599e4e03468b21b8cbe6d8310
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-12-06 22:35:33 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-# Write a script that lists all states from the database hbtn_0e_0_usa
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
-
-def main():
-    """Get variables, connect to mysql and run the query """
-    from sys import argv
-    import MySQLdb
-
-    username = str(argv[1])
-    password = str(argv[2])
-    db_name = str(argv[3])
-
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name,
-                         charset="utf8")
-    con = db.cursor()
-    con.execute("SELECT * FROM states")
-    rows = con.fetchall()
-    for row in rows:
-        print(row)
-    con.close()
-    db.close()
-
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    """Run it if main"""
-    main()
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
