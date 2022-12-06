98bef164a7673ea730907d2a514eb2a5c9972951
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-12-06 22:45:06 +0200

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-# Write a script that prints the first State object from the database
-# hbtn_0e_6_usa
+"""
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
 
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
+    instance = session.query(State).order_by(State.id).first()
 
-    ret = session.query(State).first()
-
-    if ret:
-        print("1: " + ret.name)
+    if instance is None:
+        print('Nothing')
     else:
-        print("Nothing")
-
-    session.close()
+        print('{0}: {1}'.format(instance.id, instance.name))
