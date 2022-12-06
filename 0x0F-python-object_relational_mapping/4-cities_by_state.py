1d0adb722fe17860c795ceb3e9c75e928f699595
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-12-06 22:37:39 +0200

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-# Write a script that lists all cities from the database hbtn_0e_4_usa
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 
-
-def main():
-    """Get variables, connect to mysql and run the query """
-    from sys import argv
-    import MySQLdb
-
-    username = str(argv[1])
-    password = str(argv[2])
-    db_name = str(argv[3])
-
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name,
-                         charset="utf8")
-    con = db.cursor()
-    sql_query = "SELECT cities.id, cities.name, states.name FROM cities " +\
-                "JOIN states ON cities.state_id = states.id " +\
-                "ORDER BY cities.id ASC;"
-    con.execute(sql_query)
-    rows = con.fetchall()
-    for row in rows:
-        print(row)
-    con.close()
-    db.close()
-
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    """Run it if main"""
-    main()
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
